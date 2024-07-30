a3ac561f96b29d970b5badbb03cff095c5793168
dima92 <dima-tkachenko@mail.ru>
2024-07-30 04:36:44 +0300

    config for structure project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles/index.scss';
+import './assets/fonts/Montserrat-Regular.ttf';
+
+const app = document.getElementById('app');
+
+app.innerHTML = `<div class="container">${document.title}</div>`;
